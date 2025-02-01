8dfd2a3ea52260854f17c56644392ecca57e5f1e
Ashwin Dotwal <ashwindotwal09@gmail.com>
2025-02-01 14:32:15 +0530

    add

---
+++ To-Do-List-App/main.js
@@ -0,0 +1,17 @@
+const inputBox=document.getElementById("input-box");
+const listcontainer=document.getElementById("List-container");
+
+function addTask(){
+  if(inputBox.value === ''){
+    alert("You must write something!");
+  }
+  else{
+    let li=document.createElement("li");
+    li.innerHTML=inputBox.value;
+    listcontainer.appendChild(li);
+    let span=document.createElement("span");
+    span.innerHTML="\u00d7";
+    li.appendChild(span);
+  }
+  inputBox.value ="";
+}
